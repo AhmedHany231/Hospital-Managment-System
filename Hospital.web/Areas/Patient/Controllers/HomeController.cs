a1d2e4df489deaec3fcfd20dc373cfaa1990845c
ziad <ziadkassem321@gmail.com>
2024-10-11 18:03:09 +0300

    Add project files.

---
+++ Hospital.web/Areas/Patient/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital.web.Areas.Patient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
